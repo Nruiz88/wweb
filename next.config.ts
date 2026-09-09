aeb46b771871ee8c0451b6ea01f3f6e74d1f22a5
Nicolas <niconqn.88@gmail.com>
2026-09-09 16:42:49 -0300

    chore(deploy): Dockerfile + standalone para Dokploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Requerido para Docker/Dokploy: genera .next/standalone con server.js
+  output: "standalone",
 };
 
 export default nextConfig;
